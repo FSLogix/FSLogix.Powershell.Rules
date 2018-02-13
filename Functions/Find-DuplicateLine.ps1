38b82f9fb661d9ce6d53d120da3e0c61b1e03138
JimMoyle <github@jimmoyle.com>
2018-02-13 19:30:18 +0000

    Find-DuplicateLine ignore non path lines

--- Functions/Find-DuplicateLine.ps1
+++ Functions/Find-DuplicateLine.ps1
@@ -17,12 +17,14 @@ function Find-DuplicateLine {
         [System.String[]]$HidableAppHidingRule
     )
 
-    $visibleRulesOnly = $VisibleAppHidingRule | Where-Object { $_.Startswith('HKLM\SOFTWARE\') -eq $true }
-    $hidingRulesOnly = $HidableAppHidingRule | Where-Object { $_.Startswith('HKLM\SOFTWARE\') -eq $true }
+    #$visibleRulesOnly = $VisibleAppHidingRule | Where-Object { $_.Startswith('HKLM\SOFTWARE\') -eq $true }
+    $visibleRulesOnly = $VisibleAppHidingRule | Where-Object { $_.Startswith('##') -eq $false -or $_ -ne '1' }
+    #$hidingRulesOnly = $HidableAppHidingRule | Where-Object { $_.Startswith('HKLM\SOFTWARE\') -eq $true }
+    $hidingRulesOnly = $HidableAppHidingRule | Where-Object { $_.Startswith('##') -eq $false -or $_ -ne '1' }
 
     $rules = $visibleRulesOnly + $hidingRulesOnly
 
-    $dupes = $rules | Group-Object | Where-Object {$_.Count -gt 1 } | Select-Object -ExpandProperty Name
+    $dupes = $rules | Group-Object | Where-Object { $_.Count -gt 1 } | Select-Object -ExpandProperty Name
 
     Write-Output $dupes
 }
