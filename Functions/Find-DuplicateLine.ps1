aaa1a0aa3b93aa4d8a0970d485ae915b938fed50
JimMoyle <github@jimmoyle.com>
2018-02-13 20:27:46 +0000

    Made duplicate line test more generic

--- Functions/Find-DuplicateLine.ps1
+++ Functions/Find-DuplicateLine.ps1
@@ -17,9 +17,8 @@ function Find-DuplicateLine {
         [System.String[]]$HidableAppHidingRule
     )
 
-    #$visibleRulesOnly = $VisibleAppHidingRule | Where-Object { $_.Startswith('HKLM\SOFTWARE\') -eq $true }
+    #Eliminate non relevant lines
     $visibleRulesOnly = $VisibleAppHidingRule | Where-Object { $_.Startswith('##') -eq $false -and $_ -ne '1' }
-    #$hidingRulesOnly = $HidableAppHidingRule | Where-Object { $_.Startswith('HKLM\SOFTWARE\') -eq $true }
     $hidingRulesOnly = $HidableAppHidingRule | Where-Object { $_.Startswith('##') -eq $false -and $_ -ne '1' }
 
     $rules = $visibleRulesOnly + $hidingRulesOnly
