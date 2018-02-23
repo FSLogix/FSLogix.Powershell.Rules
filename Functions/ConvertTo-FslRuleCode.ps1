5af56a84f42be99fb982da2ba8a49aec8577329c
JimMoyle <github@jimmoyle.com>
2018-02-23 11:14:28 +0000

    Added switch statement ot convert to code

--- Functions/ConvertTo-FslRuleCode.ps1
+++ Functions/ConvertTo-FslRuleCode.ps1
@@ -71,9 +71,39 @@ function ConvertTo-FslRuleCode {
 
     BEGIN {
         Set-StrictMode -Version Latest
+        $FRX_RULE_SRC_IS_A_DIR_OR_KEY       = 0x00000001
+        $FRX_RULE_SRC_IS_A_FILE_OR_VALUE    = 0x00000002
+        $FRX_RULE_CONTAINS_USER_VARS        = 0x00000008
+        $FRX_RULE_SHOULD_COPY_FILE          = 0x00000010
+        $FRX_RULE_IS_PERSISTANT             = 0x00000020 
+        $FRX_RULE_TYPE_REDIRECT             = 0x00000100
+        $FRX_RULE_TYPE_HIDING               = 0x00000200
+        $FRX_RULE_TYPE_HIDE_PRINTER	        = 0x00000400
+        $FRX_RULE_TYPE_SPECIFIC_DATA        = 0x00000800
+        $FRX_RULE_TYPE_JAVA                 = 0x00001000
+        $FRX_RULE_TYPE_VOLUME_AUTOMOUNT     = 0x00002000 
+        $FRX_RULE_TYPE_HIDE_FONT            = 0x00004000
+        $FRX_RULE_TYPE_MASK                 = 0x00007F00 
     } # Begin
     PROCESS {
-        
+        $codeToOutput = 
+        switch ($true){
+            $FolderOrKey        { $codeToOutput = $codeToOutput -bor $FRX_RULE_SRC_IS_A_DIR_OR_KEY }
+            $FileOrValue        { $codeToOutput = $codeToOutput -bor $FRX_RULE_SRC_IS_A_FILE_OR_VALUE }
+            $ContainsUserVar    { $codeToOutput = $codeToOutput -bor $FRX_RULE_CONTAINS_USER_VARS }
+            $CopyObject         { $codeToOutput = $codeToOutput -bor $FRX_RULE_SHOULD_COPY_FILE }
+            $Persistent         { $codeToOutput = $codeToOutput -bor $FRX_RULE_IS_PERSISTANT }
+            $Redirect           { $codeToOutput = $codeToOutput -bor $FRX_RULE_TYPE_REDIRECT }
+            $Hiding             { $codeToOutput = $codeToOutput -bor $FRX_RULE_TYPE_HIDING }
+            $HidePrinter        { $codeToOutput = $codeToOutput -bor $FRX_RULE_TYPE_HIDE_PRINTER }
+            $SpecificData       { $codeToOutput = $codeToOutput -bor $FRX_RULE_TYPE_SPECIFIC_DATA }
+            $Java               { $codeToOutput = $codeToOutput -bor $FRX_RULE_TYPE_JAVA }
+            $VolumeAutomount    { $codeToOutput = $codeToOutput -bor $FRX_RULE_TYPE_VOLUME_AUTOMOUNT }
+            $HideFont           { $codeToOutput = $codeToOutput -bor $FRX_RULE_TYPE_HIDE_FONT }
+            $Mask               { $codeToOutput = $codeToOutput -bor $FRX_RULE_TYPE_MASK }
+        }
+
+        Write-Output $codeToOutput
     } #Process
     END {} #End
 }  #function Convert-FslToRuleCode
