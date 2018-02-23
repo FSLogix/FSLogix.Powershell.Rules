0876e6c876a99932d55ac9553fc5cf3561d9f7fe
JimMoyle <github@jimmoyle.com>
2018-02-23 13:07:33 +0000

    Spacing

--- Functions/ConvertFrom-FslRuleCode.ps1
+++ Functions/ConvertFrom-FslRuleCode.ps1
@@ -13,23 +13,24 @@ function ConvertFrom-FslRuleCode {
 
     BEGIN {
         Set-StrictMode -Version Latest
-        $FRX_RULE_SRC_IS_A_DIR_OR_KEY       = 0x00000001
-        $FRX_RULE_SRC_IS_A_FILE_OR_VALUE    = 0x00000002
-        $FRX_RULE_CONTAINS_USER_VARS        = 0x00000008
-        $FRX_RULE_SHOULD_COPY_FILE          = 0x00000010
-        $FRX_RULE_IS_PERSISTANT             = 0x00000020 #Ask what this means
-        $FRX_RULE_TYPE_REDIRECT             = 0x00000100
-        $FRX_RULE_TYPE_HIDING               = 0x00000200
-        $FRX_RULE_TYPE_HIDE_PRINTER	        = 0x00000400
-        $FRX_RULE_TYPE_SPECIFIC_DATA        = 0x00000800 #Specific Value Rule
-        $FRX_RULE_TYPE_JAVA                 = 0x00001000
-        $FRX_RULE_TYPE_VOLUME_AUTOMOUNT     = 0x00002000 #Ask what this means
-        $FRX_RULE_TYPE_HIDE_FONT            = 0x00004000
+        $FRX_RULE_SRC_IS_A_DIR_OR_KEY = 0x00000001
+        $FRX_RULE_SRC_IS_A_FILE_OR_VALUE = 0x00000002
+        $FRX_RULE_CONTAINS_USER_VARS = 0x00000008
+        $FRX_RULE_SHOULD_COPY_FILE = 0x00000010
+        $FRX_RULE_IS_PERSISTANT = 0x00000020 #Ask what this means
+        $FRX_RULE_TYPE_REDIRECT = 0x00000100
+        $FRX_RULE_TYPE_HIDING = 0x00000200
+        $FRX_RULE_TYPE_HIDE_PRINTER = 0x00000400
+        $FRX_RULE_TYPE_SPECIFIC_DATA = 0x00000800 #Specific Value Rule
+        $FRX_RULE_TYPE_JAVA = 0x00001000
+        $FRX_RULE_TYPE_VOLUME_AUTOMOUNT = 0x00002000 #Ask what this means
+        $FRX_RULE_TYPE_HIDE_FONT = 0x00004000
         #$FRX_RULE_TYPE_MASK                 = 0x00007F00 #Ask what this means
     } # Begin
+
     PROCESS {
 
-        switch ($true){
+        switch ($true) {
             { $RuleCode -band $FRX_RULE_SRC_IS_A_DIR_OR_KEY } { $folderOrKey = $true }
             { -not ( $RuleCode -band $FRX_RULE_SRC_IS_A_DIR_OR_KEY ) } { $folderOrKey = $false}
             { $RuleCode -band $FRX_RULE_SRC_IS_A_FILE_OR_VALUE } {$fileOrValue = $true}
