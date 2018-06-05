fde23b1a6e0ba2350112276b8d26409734afa8c2
JimMoyle <github@jimmoyle.com>
2018-06-05 23:52:41 +0100

    Started help

--- FSLogix.PowerShell.Rules/Public/Set-FslAssignment.ps1
+++ FSLogix.PowerShell.Rules/Public/Set-FslAssignment.ps1
@@ -1,6 +1,40 @@
 function Set-FslAssignment {
     [CmdletBinding()]
 
+	<#
+    .SYNOPSIS
+        Function to set the content of a FSLogix Rule assignment file.
+
+    .DESCRIPTION
+        A detailed description of the function or script. This keyword can be used only once in each topic.
+
+    .PARAMETER AssignmentFilePath
+        The description of a parameter. Add a .PARAMETER keyword for
+        each parameter in the function or script syntax.
+        Type the parameter name on the same line as the .PARAMETER keyword.
+        Type the parameter description on the lines following the .PARAMETER
+        keyword. Windows PowerShell interprets all text between the .PARAMETER
+        line and the next keyword or the end of the comment block as part of
+        the parameter description. The description can include paragraph breaks.
+        The Parameter keywords can appear in any order in the comment block, but
+        the function or script syntax determines the order in which the parameters
+        (and their descriptions) appear in help topic. To change the order,
+        change the syntax.
+        You can also specify a parameter description by placing a comment in the
+        function or script syntax immediately before the parameter variable name.
+        If you use both a syntax comment and a Parameter keyword, the description
+        associated with the Parameter keyword is used, and the syntax comment is
+        ignored.
+
+    .PARAMETER RuleSetApplies
+
+    .EXAMPLE
+        A sample command that uses the function or script, optionaly followed
+        by sample output and a description. Repeat this keyword for each example.
+    #>
+
+
+
     Param (
         [Parameter(
             Position = 0,
