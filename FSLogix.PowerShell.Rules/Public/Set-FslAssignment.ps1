d1d0969f98eb142728ab438e53113a8684be1f37
JimMoyle <github@jimmoyle.com>
2018-06-06 10:57:49 +0100

    More help updates

--- FSLogix.PowerShell.Rules/Public/Set-FslAssignment.ps1
+++ FSLogix.PowerShell.Rules/Public/Set-FslAssignment.ps1
@@ -2,64 +2,63 @@ function Set-FslAssignment {
     [CmdletBinding()]
 
 	<#
-    .SYNOPSIS
-        Function to set the content of a FSLogix Rule assignment file.  This will overwrite the contents of an existing file
+        .SYNOPSIS
+            Sets the content of a FSLogix Rule assignment file.
 
-    .DESCRIPTION
-        A detailed description of the function or script. This keyword can be used only once in each topic.
+        .DESCRIPTION
+            This function can set an FSLogix assignment file contents, the assignment file should have the same basename as the matching rule file.
+            This will overwrite the contents of an existing file.
 
-    .PARAMETER AssignmentFilePath
-        The description of a parameter. Add a .PARAMETER keyword for
-        each parameter in the function or script syntax.
-        Type the parameter name on the same line as the .PARAMETER keyword.
-        Type the parameter description on the lines following the .PARAMETER
-        keyword. Windows PowerShell interprets all text between the .PARAMETER
-        line and the next keyword or the end of the comment block as part of
-        the parameter description. The description can include paragraph breaks.
-        The Parameter keywords can appear in any order in the comment block, but
-        the function or script syntax determines the order in which the parameters
-        (and their descriptions) appear in help topic. To change the order,
-        change the syntax.
-        You can also specify a parameter description by placing a comment in the
-        function or script syntax immediately before the parameter variable name.
-        If you use both a syntax comment and a Parameter keyword, the description
-        associated with the Parameter keyword is used, and the syntax comment is
-        ignored.
+        .PARAMETER AssignmentFilePath
+            The description of a parameter. Add a .PARAMETER keyword for
+            each parameter in the function or script syntax.
+            Type the parameter name on the same line as the .PARAMETER keyword.
+            Type the parameter description on the lines following the .PARAMETER
+            keyword. Windows PowerShell interprets all text between the .PARAMETER
+            line and the next keyword or the end of the comment block as part of
+            the parameter description. The description can include paragraph breaks.
+            The Parameter keywords can appear in any order in the comment block, but
+            the function or script syntax determines the order in which the parameters
+            (and their descriptions) appear in help topic. To change the order,
+            change the syntax.
+            You can also specify a parameter description by placing a comment in the
+            function or script syntax immediately before the parameter variable name.
+            If you use both a syntax comment and a Parameter keyword, the description
+            associated with the Parameter keyword is used, and the syntax comment is
+            ignored.
 
-    .PARAMETER RuleSetApplies
+        .PARAMETER RuleSetApplies
+            This determines whether a ruleset does or does not apply to users/groups/processes etc.  For instance when using a Hiding rule, applying that hiding rule to users will hide the file from the users assigned to it when applied.
+        .PARAMETER UserName
+            If you wish to tie down the rule to an individual user use theier unsername in this parameter.  Groupname is more usual for assignments however
+        .PARAMETER GroupName
+            Use this to tie the assignment of the rule to a specific group
+        .PARAMETER WellKnownSID
+            The Well Known SID for groups such as Domain Admins are useful for cross-language 
+        .PARAMETER ADDistinguisedName
 
-    .PARAMETER UserName
+        .PARAMETER ProcessName
 
-    .PARAMETER GroupName
+        .PARAMETER IncludeChildProcess
 
-    .PARAMETER WellKnownSID
+        .PARAMETER ProcessId
 
-    .PARAMETER ADDistinguisedName
+        .PARAMETER IPAddress
 
-    .PARAMETER ProcessName
+        .PARAMETER ComputerName
 
-    .PARAMETER IncludeChildProcess
+        .PARAMETER OU
 
-    .PARAMETER ProcessId
+        .PARAMETER EnvironmentVariable
 
-    .PARAMETER IPAddress
+        .PARAMETER AssignedTime
 
-    .PARAMETER ComputerName
+        .PARAMETER UnAssignedTime
 
-    .PARAMETER OU
-
-    .PARAMETER EnvironmentVariable
-
-    .PARAMETER AssignedTime
-
-    .PARAMETER UnAssignedTime
-
-    .EXAMPLE
-        A sample command that uses the function or script, optionaly followed
-        by sample output and a description. Repeat this keyword for each example.
+        .EXAMPLE
+            A sample command that uses the function or script, optionaly followed
+            by sample output and a description. Repeat this keyword for each example.
     #>
-
-
 
     Param (
         [Parameter(
