d003e8a5ad505b4ecf3252734de429d7c10cba57
JimMoyle <github@jimmoyle.com>
2018-07-16 17:38:44 -0700

    imprived integration tests

--- FSLogix.PowerShell.Rules/tests/QA/Integration.Tests.ps1
+++ FSLogix.PowerShell.Rules/tests/QA/Integration.Tests.ps1
@@ -1,37 +1,34 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 #$funcType = Split-Path $here -Leaf
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
-$here = $here | Split-Path -Parent | Split-Path -Parent
+$global:here = $here | Split-Path -Parent | Split-Path -Parent
 #. "$here\$funcType\$sut"
 
-Import-Module -Name (Join-Path $here 'FSLogix.PowerShell.Rules.psd1') -Force
+Import-Module -Name (Join-Path $global:here 'FSLogix.PowerShell.Rules.psd1') -Force
 
 InModuleScope 'FSLogix.PowerShell.Rules' {
 
     Describe Compare-FslRuleFile {
 
-        BeforeAll {
-            [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseDeclaredVarsMoreThanAssignments", "")]
-            $outputPath = 'TestDrive:\'
+        AfterAll {
+            Remove-Variable -Name 'here' -Scope Global
         }
 
-        It 'Produces 6 files' {
-            New-Item -Path $outputPath -Name "Office2016" -ItemType "directory"
-            Compare-FslRuleFile -Files (Get-ChildItem (Join-Path $here tests\QA\TestFiles\Office2016)).FullName -OutputPath (Join-Path $outputPath 'Office2016')
-                (Get-ChildItem (Join-Path $outputPath 'Office2016')).count | Should Be 6
+        $outputPath = 'TestDrive:\'
+
+        $samplePath = Join-Path -Path $global:here -ChildPath tests\QA\TestFiles\CustomerSamples
+
+        foreach ($folder in Get-ChildItem -Path $samplePath -Directory) {
+
+            $files = Get-ChildItem -Path $folder.FullName -Filter "*.fxr"
+
+            It "Produces $($files.count * 2) files for $($folder.name)" {
+                $files = Get-ChildItem -Path $folder.FullName -Filter "*.fxr"
+                New-Item -Path $outputPath -Name $folder.name -ItemType "directory"
+                Compare-FslRuleFile -Files $files.FullName -OutputPath (Join-Path $outputPath $folder.name)
+                (Get-ChildItem (Join-Path $outputPath $folder.name)).count | Should Be ($files.count * 2)
+
             }
-
-        It 'Produces 6 files' {
-            New-Item -Path $outputPath -Name "Office2013" -ItemType "directory"
-            Compare-FslRuleFile -Files (Get-ChildItem (Join-Path $here tests\QA\TestFiles\Office2013)).FullName -OutputPath (Join-Path $outputPath 'Office2013')
-            ( Get-ChildItem (Join-Path $outputPath 'Office2013') -File ).count | Should Be 6
         }
-
-        It 'Produces 4 files' {
-            New-Item -Path $outputPath -Name "Office2016Pro" -ItemType "directory"
-            Compare-FslRuleFile -Files (Get-ChildItem (Join-Path $here tests\QA\TestFiles\Office2016Pro)).FullName -OutputPath (Join-Path $outputPath 'Office2016Pro')
-            ( Get-ChildItem (Join-Path $outputPath 'Office2016Pro') -File ).count | Should Be 4
-        }
-
     }
 }
