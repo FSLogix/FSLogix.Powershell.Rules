cab9ad795b8dba62e6d42b7b03d537674979ea78
JimMoyle <github@jimmoyle.com>
2019-06-12 19:06:23 +0100

    Commented out unused variables in Pester tests

--- FSLogix.PowerShell.Rules/tests/QA/RegHexPipelineTests.Tests.ps1
+++ FSLogix.PowerShell.Rules/tests/QA/RegHexPipelineTests.Tests.ps1
@@ -1,6 +1,6 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$funcType = Split-Path $here -Leaf
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+#$funcType = Split-Path $here -Leaf
+#$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 $here = $here | Split-Path -Parent | Split-Path -Parent | Split-Path -Parent
 . "$here\FSLogix.PowerShell.Rules\Private\ConvertFrom-FslRegHex.ps1"
 . "$here\FSLogix.PowerShell.Rules\Private\ConvertTo-FslRegHex.ps1"
--- FSLogix.PowerShell.Rules/tests/QA/RuleFilePipeline.Tests.ps1
+++ FSLogix.PowerShell.Rules/tests/QA/RuleFilePipeline.Tests.ps1
@@ -1,6 +1,6 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 #$funcType = Split-Path $here -Leaf
-$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+#$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 $global:here = $here |  Split-Path -Parent | Split-Path -Parent
 #. "$here\$funcType\$sut"
 
