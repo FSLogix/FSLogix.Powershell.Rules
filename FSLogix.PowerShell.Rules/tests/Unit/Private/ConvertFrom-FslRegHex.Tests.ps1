003f227c1e66acb4f2f887f8701ac3afbbb716ff
JimMoyle <github@jimmoyle.com>
2019-06-24 19:41:59 +0100

    extra tests

--- FSLogix.PowerShell.Rules/tests/Unit/Private/ConvertFrom-FslRegHex.Tests.ps1
+++ FSLogix.PowerShell.Rules/tests/Unit/Private/ConvertFrom-FslRegHex.Tests.ps1
@@ -6,12 +6,20 @@ $here = $here | Split-Path -Parent | Split-Path -Parent | Split-Path -Parent
 
 Describe $sut.TrimEnd('.ps1') {
 
-    It "Returns correct result for String" {
-        ConvertFrom-FslRegHex -HexString 014300680061006E0067006500640057006900740068004700750069000000 -RegValueType String | Should -Be 'ChangedWithGui'
+    It "Returns correct data for a String" {
+        (ConvertFrom-FslRegHex -HexString 014300680061006E0067006500640057006900740068004700750069000000).Data | Should -Be 'ChangedWithGui'
     }
 
-    It "Returns correct result for DWORD" {
-        ConvertFrom-FslRegHex -RegData '042D000000' -RegValueType DWORD | should -Be 45
+    It "Returns correct data for a DWORD" {
+        (ConvertFrom-FslRegHex -HexString 042D000000).Data | should -Be 45
+    }
+
+    It "Returns correct type for String" {
+        (ConvertFrom-FslRegHex -HexString 014300680061006E0067006500640057006900740068004700750069000000).RegValueType | Should -Be 'String'
+    }
+
+    It "Returns correct type for DWORD" {
+        (ConvertFrom-FslRegHex -HexString 042D000000).RegValueType | should -Be 'DWORD'
     }
 
 }
