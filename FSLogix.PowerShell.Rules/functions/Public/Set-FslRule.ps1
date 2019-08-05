6843d266798d8df03030796ea4d4105c377e58a8
JimMoyle <github@jimmoyle.com>
2019-08-05 15:44:53 +0100

    Export module member correct format, added alias to set-fslrule

--- FSLogix.PowerShell.Rules/functions/Public/Set-FslRule.ps1
+++ FSLogix.PowerShell.Rules/functions/Public/Set-FslRule.ps1
@@ -8,7 +8,8 @@ function Set-FslRule {
             Mandatory = $true,
             ValuefromPipelineByPropertyName = $true
         )]
-        [System.String]$RuleFilePath,
+        [Alias('RuleFilePath')]
+        [System.String]$Path,
 
         [Parameter(
             ParameterSetName = 'Hiding',
